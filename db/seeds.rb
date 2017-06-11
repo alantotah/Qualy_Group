7adc8d9467a68bd96777631311fdb18836ac0f60
alantotah <alantotah@users.noreply.github.com>
2017-06-11 11:12:16 -0500

    v9 FFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'ffaker'
-User.all.destroy
+User.destroy_all
 
 10.times do
-  name = FFaker::Name.name
-  email = FFaker::Internet.email
-  telefono = FFaker::PhoneNumber.phone_number
-  password = FFaker::Internen.password
+  name = FFaker::NameMX.male_name
+  appellido = FFaker::Name.last_name
+  puesto = FFaker::Job.title
+  email = FFaker::Internet.safe_email
+  telefono = FFaker::PhoneNumberMX.phone_number
+  password = "12341234"
+  direccion = FFaker::Address.street_address
+  nombre_empresa = FFaker::Company.name
   User.create!( nombre: name,
   email: email,
   telefono: telefono,
   password: password,
-  password_confirmation: password)
+  password_confirmation: password,
+  direccion: direccion,
+  nombre_empresa: nombre_empresa,
+  puesto: puesto)
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+if AdminUser.find_by(email: "admin@example.com")==nil
+  AdminUser.create!(email:"admin@example.com", password: "password", password_confirmation: "password")
+end
