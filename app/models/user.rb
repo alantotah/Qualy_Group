fd3f48865aedbe12da6bd3d086c1a58839ed581a
alantotah <alantotah@users.noreply.github.com>
2017-06-13 14:22:23 -0400

    v11

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :orders, :dependent => :destroy
 
-  # validates :telefono, :presence => true
-  # validates :direccion, :presence => true
-  # validates :nombre_empresa, :presence => true
-  # validates :nombre_empresa, :uniqueness => true
-  # validates :nombre, :presence => true
-  # validates :appellido, :presence => true
+  validates :telefono, :presence => true
+  validates :direccion, :presence => true
+  validates :nombre_empresa, :presence => true
+  validates :nombre_empresa, :uniqueness => true
+  validates :nombre, :presence => true
+  validates :appellido, :presence => true
 
 
   devise :database_authenticatable, :registerable,
